d707e04d7ab13c38969b46cd6b4ceaf872dda999
aldi <iskandar.aldi88@gmail.com>
2024-08-01 14:12:10 +0700

    [main] adding status code

--- pkg/contants/errors.go
+++ pkg/contants/errors.go
@@ -18,6 +18,18 @@ var errorMessages = map[ErrorCode]string{
 	Unauthorized:        "unauthorized",
 }
 
+var statusCodes = map[ErrorCode]int{
+	NoError:             200,
+	InternalServerError: 500,
+	InvalidRequest:      400,
+	NotFound:            404,
+	Unauthorized:        401,
+}
+
 func (e ErrorCode) Error() string {
 	return errorMessages[e]
+}
+
+func (e ErrorCode) StatusCode() int {
+	return statusCodes[e]
 }
